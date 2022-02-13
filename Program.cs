647b1c63250f0528006c550a51a2cb36fabf5d1c
David Pallmann <dpallmann@hotmail.com>
2022-02-13 16:58:58 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Amazon;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using System.IO;
+
+RegionEndpoint region = RegionEndpoint.USWest2;
+
+if (args.Length >= 3)
+{
+    var pollyClient = new AmazonPollyClient(region);
+
+    var languageCode = args[0];
+    var voiceId = args[1];
+    var text =   args[2];
+
+    if (text.Contains("/") || text.Contains(@"\"))
+    {
+        text = File.ReadAllText(text);
+    }
+
+    Console.WriteLine($"Sending request to Amazon Polly to synthesize speech from text: {text}");
+
+    var isSSML = text.StartsWith("<speak>");
+
+    var request = new SynthesizeSpeechRequest
+    {
+        LanguageCode = languageCode,
+        VoiceId = (VoiceId)voiceId,
+        Text = text,
+        OutputFormat = OutputFormat.Mp3,
+        LexiconNames = (args.Length > 3) ? new List<string>(args[4].Split(',')) : null,
+        TextType = isSSML ? TextType.Ssml : TextType.Text,
+        Engine = isSSML ? Engine.Standard : Engine.Neural
+    };
+
+    var response = await pollyClient.SynthesizeSpeechAsync(request);
+
+    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+    {
+        Console.WriteLine($"Synthesized successfully");
+
+        var mp3Filename = (@"polly-tts.mp3");
+
+        using (var fileStream = File.Create(mp3Filename))
+        {
+            response.AudioStream.CopyTo(fileStream);
+            fileStream.Flush();
+            fileStream.Close();
+        }
+
+        Console.WriteLine($"Audio stream saved to {mp3Filename}");
+    }
+    else
+    {
+        Console.WriteLine($"Error: SynthesizeSpeechAsync returned HTTP status code {response.HttpStatusCode}");
+    }
+}
+else
+{
+    Console.WriteLine("Usage: dotnet run -- languageCode voiceCode text|file [lexicon-names]");
+    Console.WriteLine("Ex:    dotnet run -- en-US Mike \"Hello there. My name is Mike.\"");
+    Console.WriteLine("Ex:    dotnet run -- en-US Amy \"data\\article.txt\"");
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello, Polly!
+
+This is the code project for the [Hello, Polly!](https://davidpallmann.hashnode.dev/hello-polly) blog post. 
+
+This episode: Amazon Polly and Text-to-Speech. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce Amazon Polly and use it in a "Hello, Cloud" .NET program to perform Text-to-Speech. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+
+## Our Hello, Polly Project
+
+After getting familiar with Amazon Polly in the AWS console, you'll write a .NET 6 console program that converts text or SSML to synthesized speech, saved as an MP3 file.
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
